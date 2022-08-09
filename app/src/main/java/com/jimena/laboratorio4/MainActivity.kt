5538a210a278a9c64c10280577f68f2355785fce
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2022-08-08 18:16:13 -0600

    cambios

---
+++ app/src/main/java/com/jimena/laboratorio4/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.jimena.laboratorio4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+
+class MainActivity : AppCompatActivity() {
+    //Declaracion de variables
+    lateinit var btnToNextActivity: android.widget.Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnToNextActivity = findViewById(R.id.button_MainActivity_Terminarjornada)
+        initListeners()
+    }
+    //Inicializacion de listeners
+    fun initListeners() {
+        btnToNextActivity.setOnClickListener {
+            val intent = android.content.Intent(this, LinearActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}

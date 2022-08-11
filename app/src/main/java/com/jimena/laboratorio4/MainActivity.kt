c5da6c87cd54769f57da9c3f8c8b04a53ae68eac
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2022-08-10 21:11:24 -0600

    Toastmessage

--- app/src/main/java/com/jimena/laboratorio4/MainActivity.kt
+++ app/src/main/java/com/jimena/laboratorio4/MainActivity.kt
@@ -3,23 +3,28 @@ package com.jimena.laboratorio4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
     //Declaracion de variables
     lateinit var btnToNextActivity: Button //lateinit es para que no se genere un error de que no se ha inicializado la variable
-
+    lateinit var btnTerminarjornada: Button
     override fun onCreate(savedInstanceState: Bundle?) { //onCreate es el primer metodo que se ejecuta al iniciar la actividad
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnToNextActivity = findViewById(R.id.button_MainActivity_Terminarjornada)
+        btnToNextActivity = findViewById(R.id.button3)
+        btnTerminarjornada= findViewById(R.id.button_MainActivity_Terminarjornada)
         initListeners()
     }
     //Inicializacion de listeners
     fun initListeners() {
+
         btnToNextActivity.setOnClickListener {
-            val intent = android.content.Intent(this, LinearActivity::class.java)
-            startActivity(intent)
+            Toast.makeText(this, "Karen Jimena Hernández Ortega", Toast.LENGTH_LONG).show()
+        }
+        btnTerminarjornada.setOnClickListener {
+            Toast.makeText(this, "Jornada finalizada", Toast.LENGTH_LONG).show()
         }
     }
 }
